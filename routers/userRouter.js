b2cb36a1a4b14a4b4494e43f241c7531e6dcb21d
yuvalBerghaus <yuvalBerghaus>
2023-05-03 19:28:53 +0300

    updated

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -6,5 +6,4 @@ userRouter.get('/', userController.getAllUsers);
 userRouter.get('/:email', userController.getUserByEmail);
 userRouter.post('/', userController.addUser); // Add this line to create the new route
 userRouter.post('/login', userController.loginUser); // Add this line to create the new route
-
 module.exports = { userRouter };
